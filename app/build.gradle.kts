5407a41d5038b614b992f2fc653dc1aa8616ee1e
Nadun Gawesh <nadungawesh@Naduns-MacBook-Air.local>
2026-09-19 13:00:39 +0530

    Release v1.4.0 (build 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "lk.novalink.zerotrace"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.3.1"
+        versionCode = 24
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
